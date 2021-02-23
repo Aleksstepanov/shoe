2177636909e7a6fa33e12d6a8aa315be071fae26
Aleksey <stepanovl87@gmail.com>
2021-02-23 07:07:25 +0300

    cabinet

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import './style/app.scss';
 import './img/logo.svg';
 import './img/mainBackground.svg';
 import './img/close.svg';
+import './img/cabinet1.svg';
+import './img/cabinet2.svg';
+import './img/cabinet3.svg';
+import './img/card__footer.svg';
